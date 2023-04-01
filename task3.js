3c43bd5a90acf6bfa3cf4b6a66dab8b937805f89
makeum <jo_god_da@inha.edu>
2023-04-01 15:46:04 +0900

    solution

--- task3.js
+++ task3.js
@@ -1,1 +1,10 @@
 /* 3번 문제 답안을 작성해주세요. */
+function solution(n) {
+    var result = 1;
+    let i=1  
+    while(result * i <=n){
+        result*=i; // 2
+        i+=1 // 3
+    }
+    return i-1;
+}
--- task4.js
+++ task4.js
@@ -1,1 +1,6 @@
 /* 4번 문제 답안을 작성해주세요. */
+function solution(M, N) {
+    var answer = 0;
+    answer=(M*N)-1
+    return answer;
+}git
