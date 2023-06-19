de3a8f5d20da4c55a8b9145490d0a9a1ea7d2025
makeum <jo_god_da@inha.edu>
2023-06-19 17:00:21 +0900

    20230619 복습 완료

--- task2.js
+++ task2.js
@@ -1,12 +1,9 @@
 /* 2번 문제 답안을 작성해주세요. */
 function solution(num_list) {
-    var answer = [0,0];
-    let num1=0;
-    let num2=0;
-    num_list.map((num)=>{
-        if (num%2===0) answer[0]++;
-        else answer[1]++; //else 다음에는 조건식 안 써도 돼 
-        return num //map 함수를 쓰려고 num 매개변수를 넣어줬어 
+    let answer = [0, 0];
+    num_list.map((num) => {
+        if (num % 2 === 0) answer[0]++;
+        else answer[1]++;
     })
     return answer;
 }
--- task3.js
+++ task3.js
@@ -1,10 +1,11 @@
 /* 3번 문제 답안을 작성해주세요. */
 function solution(n) {
-    var result = 1;
-    let i=1  
-    while(result * i <=n){
-        result*=i; // 2
-        i+=1 // 3
+    let answer = 1;
+    let i = 1;
+    while (i * answer <= n){
+        answer *= i
+        i += 1 
     }
-    return i-1;
+    
+    return i - 1; //마지막 i가 1이 더해진 상태로 반환될 테니 -1을 해줘야 조건에 맞는 i값을 return할 수 있다 
 }
